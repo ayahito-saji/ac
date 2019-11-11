2d5d93b12eb2016fa92f97ca9bcb76cf73521e96
Saji Ayahito <ayahito_saji@outlook.jp>
2019-11-11 16:02:29 +0900

    modify

--- B.cpp
+++ B.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
+#include <iomanip> // setprecision
+#include <cmath>
 
 using namespace std;
+typedef long long ll;
+const double long PI = acos(-1);
 
-int main(void) {
-  int v;
+void solve() {
+  ll v;
   cin >> v;
   cout << v << endl;
+}
+
+int main(void) {
+  cout << fixed << setprecision(10);
+  solve();
+  return 0;
 }
